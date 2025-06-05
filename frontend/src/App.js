2c850643997bf1b62718a69775baf3669d901eff
Hanvitha Gavini <gavini.honey@gmail.com>
2025-06-05 18:36:47 -0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1 className="demoproject">AI-driven Energy Savings for RAN </h1>
+      <h1 className="demoproject"> Kepler Chatbot </h1>
       <Chatbot />
       <Refresh />
     </div>
